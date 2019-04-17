64981dd6647fe27553704c15c29771db0bcb95d1
Erik Sandstrom (exjob Ted) <eriks@maths.lth.se>
2019-04-17 15:14:52 +0200

    Original growGAN wo layer growth

---
+++ OJ_grow_celebA/grow_gan/main.py
@@ -0,0 +1,152 @@
+import os
+import scipy.misc
+import numpy as np
+from glob import glob
+
+from growGAN import growGAN
+from utils import pp, visualize, show_all_variables
+
+import tensorflow as tf
+
+z_dims = '8.16.32.64.128.256'
+epochs = '1.4.4.4.4.4'
+g_layers = '12.12.12.12.12.12'
+d_layers = '13.13.13.13.13.13'
+output_dims = '128.128.128.128.128.128' 
+useAlpha = 'n.n.n.n.n.n'
+useBeta = 'n.y.y.y.y.y'
+feature_map_shrink = 'n' # ['n', 'f'] generator
+feature_map_growth = 'n' # ['n', 'f'] discriminator
+spatial_map_shrink = 'n' # ['n', 'f'] discriminator
+spatial_map_growth = 'n' # ['n', 'f'] generator
+stage = 'f.f.f.f.f.f'
+loss = 'ns' # ['RaLS', 'ns', 'wa']
+z_distr = 'u' # ['u', 'g']
+activation = 'lrelu'
+weight_init = 'z' # ['z', 'u', 'g', 'x', 'he']
+lr = 0.0002
+beta1 = 0.0
+beta2 = 0.99
+epsilon = 0.00000001
+batch_size = 1
+sample_num = 64
+gpu = 1
+g_batchnorm = True
+d_batchnorm = True
+normalize_z = False
+crop = True
+trainflag = True
+visualize = False
+
+
+model_dir = z_dims +'_'+ epochs +'_'+ g_layers +'_'+ d_layers +'_'+ output_dims +'_'+feature_map_shrink+feature_map_growth+spatial_map_shrink+spatial_map_growth+'_'+ loss +'_'+z_distr +'_'+ activation +'_'+ weight_init +'_'+ str(batch_size) +'_'+str(g_batchnorm) +'_'+ str(d_batchnorm) +'_'+ str(normalize_z)
+
+flags = tf.app.flags
+flags.DEFINE_string("z_dims", z_dims,
+    "List of the latent space dimension per network cycle")
+flags.DEFINE_string("epochs", epochs, 
+    "List of epochs to train each network cycle with")
+flags.DEFINE_string("g_layers", g_layers, 
+    "List of layers to train each generator network cycle with")
+flags.DEFINE_string("d_layers", d_layers, 
+    "List of layers to train each discriminator network cycle with")
+flags.DEFINE_string("output_dims", output_dims,
+    "List of output dimensions to train each generator network cycle with")
+flags.DEFINE_string("useAlpha", useAlpha,
+    "Use spatial smoothing or not")
+flags.DEFINE_string("useBeta", useBeta,
+    "Use feature channel smoothing or not")
+flags.DEFINE_string("feature_map_shrink", feature_map_shrink,
+    "How fast the nbr of feature maps should decrease in the generator")
+flags.DEFINE_string("feature_map_growth", feature_map_growth,
+    "How fast the nbr of feature maps should increase in the discriminator")
+flags.DEFINE_string("spatial_map_shrink", spatial_map_shrink,
+    "How fast the spatial size should decrease in the discriminator")
+flags.DEFINE_string("spatial_map_growth", spatial_map_growth,
+    "How fast the spatial size should increase in the generator")
+flags.DEFINE_string("stage", stage,
+    "What stage the gan is at")
+flags.DEFINE_string("loss", loss,
+    "Loss function")
+flags.DEFINE_string("z_distr", z_distr,
+    "The latent distribution")
+flags.DEFINE_string("weight_init", weight_init,
+    "Weight initialization method when growing")
+flags.DEFINE_string("activation", activation,
+    "Activation function")
+flags.DEFINE_float("lr", lr,
+    "Learning rate of for adam")
+flags.DEFINE_float("beta1", beta1, "Momentum term 1 of adam")
+flags.DEFINE_float("beta2", beta2, "Momentum term 2 of adam")
+flags.DEFINE_float("epsilon", epsilon, "Epsilon term of adam")
+flags.DEFINE_integer("batch_size", batch_size, "The size of batch images")
+flags.DEFINE_integer("sample_num", sample_num, "The size of sample images")
+flags.DEFINE_integer("gpu", gpu, "GPU to use")
+flags.DEFINE_boolean("g_batchnorm", g_batchnorm,
+                     "True using batch norm, False for not using it")
+flags.DEFINE_boolean("d_batchnorm", d_batchnorm,
+                     "True using batch norm, False for not using it")
+flags.DEFINE_boolean(
+    "normalize_z", normalize_z, "sample on a hypersphere or not")
+flags.DEFINE_boolean(
+    "crop", crop, "crop the images to appropriate size")
+flags.DEFINE_boolean(
+    "trainflag", trainflag, "True for training, False for testing")
+flags.DEFINE_boolean("visualize", visualize,
+                     "True for visualizing, test mode")
+flags.DEFINE_string("model_dir", model_dir,
+                     "Directory name to save the images/models/logs")
+
+FLAGS = flags.FLAGS
+
+
+def main(_):
+    pp.pprint(flags.FLAGS.__flags)
+
+    gan = growGAN(
+        z_dims = FLAGS.z_dims,
+        epochs = FLAGS.epochs,
+        g_layers = FLAGS.g_layers,
+        d_layers = FLAGS.d_layers,
+        output_dims = FLAGS.output_dims,
+        useAlpha = FLAGS.useAlpha,
+        useBeta = FLAGS.useBeta,
+        feature_map_shrink = FLAGS.feature_map_shrink,
+        feature_map_growth = FLAGS.feature_map_growth,
+        spatial_map_shrink = FLAGS.spatial_map_shrink,
+        spatial_map_growth = FLAGS.spatial_map_growth,
+        stage = FLAGS.stage,
+        loss = FLAGS.loss,
+        z_distr = FLAGS.z_distr,
+        activation = FLAGS.activation,
+        weight_init = FLAGS.weight_init,
+        lr = FLAGS.lr,
+        beta1 = FLAGS.beta1,
+        beta2 = FLAGS.beta2,
+        epsilon = FLAGS.epsilon,
+        batch_size = FLAGS.batch_size,
+        sample_num = FLAGS.sample_num,
+        gpu = FLAGS.gpu,
+        g_batchnorm = FLAGS.g_batchnorm,
+        d_batchnorm = FLAGS.d_batchnorm,
+        normalize_z = FLAGS.normalize_z,
+        crop = FLAGS.crop,
+        trainflag = FLAGS.trainflag,
+        visualize = FLAGS.visualize,
+        model_dir = FLAGS.model_dir) 
+
+    show_all_variables()
+
+
+    if FLAGS.trainflag:
+        gan.train()
+    else:
+       if not gan.load()[0]:
+           raise Exception("[!] Train a model first, then run test mode")
+
+    if FLAGS.visualize:
+        visualize(sess, gan, FLAGS)
+
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ OJ_grow_celebA/grow_gan/model.py
@@ -0,0 +1,237 @@
+import tensorflow as tf
+from ops import *
+from utils import *
+
+# feature_map_shrink can be normal, fast. Normal is that we decrease the feature maps by half every other layer.
+# fast is that we decrease them as late as possible, doing it for every layer when we need to.
+# spatial_map_growth can be normal, fast. Normal is that we double the spatial dimension every other layer.
+# fast is that we double the spatial dimension every layer.
+
+def G(z, batch_size=64, reuse = False, bn = True, layers = 12, activation = 'lrelu', output_dim = 128,
+    feature_map_shrink = 'n', spatial_map_growth = 'n', alpha = 1, useAlpha = 'y', beta = 1, useBeta = 'y'):
+    with tf.variable_scope("generator") as scope:
+        if reuse:
+            scope.reuse_variables()
+
+        if feature_map_shrink == 'n':
+            idx = layers
+            featureshrinkList = []
+            idx = idx - 2
+            while idx > 1:
+                featureshrinkList.append(idx)
+                idx = idx - 2
+            times_to_shrink = int(np.log2(int(z.get_shape()[-1]//8)))
+            featureshrinkList = featureshrinkList[:times_to_shrink]
+
+
+        if feature_map_shrink == 'f':
+            nbr_layers_shrink = int(z.get_shape()[-1])//8
+            idx_shrink = layers - np.log2(nbr_layers_shrink)
+            print('idx_shrink: ', idx_shrink)
+        print('input shape z:', z.get_shape())
+
+        for i in range(layers):
+            if i == 0:
+                # fully-connected layers (equivalent to 4x4 conv)
+                print('batch or sample size: ', batch_size)
+                h = conv4x4(z, int(z.get_shape()[-1])*4*4, batch_size, name = 'g_h'+str(i+1), useBeta = useBeta, beta = beta)
+                print('g_h1:', h.get_shape())
+            else:
+                if spatial_map_growth == 'n' and i % 2 == 0 and int(h.get_shape()[1]) < output_dim:
+                    h = upscale2d(h, factor=2)
+                    if i == layers - 2 and useAlpha == 'y':
+                        res_connect = h
+                elif spatial_map_growth == 'f' and int(h.get_shape()[1]) < output_dim:
+                    h = upscale2d(h, factor=2)
+                if feature_map_shrink == 'n':
+                    if i in featureshrinkList and int(h.get_shape()[-1]) > 8:
+                        if i <= layers - 2:
+                            if i == layers - 2:
+                                h = conv2d(h, int(h.get_shape()[-1])//2, 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta, last = True)
+                            else:
+                                h = conv2d(h, int(h.get_shape()[-1])//2, 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta)
+                        else:
+                            h = conv2d(h, int(h.get_shape()[-1])//2, 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                        np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+                        print('g_h'+str(i+1)+':', h.get_shape())
+                    else:
+                        if i <= layers - 2:
+                            if i == layers - 2:
+                                h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta, last = True)
+                            else:
+                                h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta)
+                        else:
+                            h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                        np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+                        print('g_h'+str(i+1)+':', h.get_shape())
+                elif feature_map_shrink == 'f':
+                    if i >= idx_shrink:
+                        if i <= layers - 2:
+                            if i == layers - 2:
+                                h = conv2d(h, int(h.get_shape()[-1])//2, 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta, last = True)
+                            else:
+                                h = conv2d(h, int(h.get_shape()[-1])//2, 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta)
+                        else:
+                            h = conv2d(h, int(h.get_shape()[-1])//2, 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                        np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+                        print('g_h'+str(i+1)+':', h.get_shape())
+                    else:
+                        if i <= layers - 2:
+                            if i == layers - 2:
+                                h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta, last = True)
+                            else:
+                                h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta)
+                        else:
+                            h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='g_h'+str(i+1), stddev = 
+                        np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+                        print('g_h'+str(i+1)+':', h.get_shape())
+
+                
+            # if bn:
+            #     g_bn = batch_norm(name='g_bn'+str(i+1))
+            #     h = g_bn(h)
+            if activation == 'lrelu':
+                h = lrelu(h)
+            elif activation == 'relu':
+                h = relu(h)
+
+        if useAlpha == 'y':
+            h = tf.add(tf.scalar_mul(1-alpha,res_connect), tf.scalar_mul(alpha,h), name = 'g_smoothed')
+            print('fused')
+        out = conv2d(h, 3, 1, 1, 1, 1, name='g_out', stddev = np.sqrt(2/(8*output_dim*output_dim)))
+
+        print('out generator shape: ', out.get_shape())
+        
+        out = tf.nn.tanh(out)
+    return out
+
+# feature_map_growth can be normal, fast. Normal is that we increase the feature maps by doubling every other layer.
+# fast is that we decrease them as early as possible, doing it for every layer up to 256.
+# spatial_map_shrink can be normal, fast. Normal is that we halve the spatial dimension every other layer.
+# fast is that we halve the spatial dimension every layer.
+
+
+def D(image, batch_size=64, reuse = False, bn = True, layers = 12, activation = 'lrelu', input_dim = 128,
+    feature_map_growth = 'n', spatial_map_shrink = 'n', stage = 'i', alpha = 1, useAlpha = 'y', beta = 1, useBeta = 'y', z_dim = 8):
+    with tf.variable_scope("discriminator") as scope:
+        if reuse:
+            scope.reuse_variables()
+
+        idx = layers
+        downsampleList = []
+        idx = idx - 2
+        while idx > 1:
+            downsampleList.append(idx)
+            idx = idx - 2
+
+        print('Indices when to downsample: ', downsampleList)
+
+        for i in range(layers):
+            if i == 0:
+                # 1x1 conv
+                h = conv2d(image, 8, 1, 1, 1, 1, name = 'd_h1')
+                if useAlpha == 'y':
+                    res_connect = h
+                    if activation == 'lrelu':
+                        res_connect = lrelu(res_connect)
+                    elif activation == 'relu':
+                        res_connect = relu(res_connect)
+                    # BATCHNORM NEEDS TO BE HERE TOO!
+                    res_connect = downscale2d(res_connect, factor = 2)
+
+                print('d_h1:', h.get_shape())
+            elif i == layers-1:
+                h = conv2d(h, int(h.get_shape()[-1]), 4, 4, 1, 1, name = 'd_h'+str(layers), padding = 'VALID', useBeta = useBeta, beta = beta)
+                print('d_h'+str(i+1)+':', h.get_shape())
+            else:
+                if spatial_map_shrink == 'n' and i in downsampleList and int(h.get_shape()[1]) > 4: #and i != 1 and (i+1) % 2 == 0
+                    h = downscale2d(h, factor=2)
+                    if useAlpha == 'y' and i == 3:
+                        h = tf.add(tf.scalar_mul(1-alpha,res_connect), tf.scalar_mul(alpha, h), name = 'd_smoothed')
+                        print('fused')
+                elif spatial_map_shrink == 'f' and int(h.get_shape()[1]) > 4:
+                    h = downscale2d(h, factor=2)
+                if feature_map_growth == 'n':
+                    if i in downsampleList and useAlpha == 'n' and int(h.get_shape()[-1]) < z_dim: # i % 2 == 0
+                        if i >= 3:
+                            if i == 3:
+                                h = conv2d(h, int(h.get_shape()[-1])*2, 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta, first = True)
+                            else:
+                                h = conv2d(h, int(h.get_shape()[-1])*2, 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta)
+                        else:
+                            h = conv2d(h, int(h.get_shape()[-1])*2, 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+                        print('d_h'+str(i+1)+':', h.get_shape())
+                    elif i in downsampleList and int(h.get_shape()[-1]) < z_dim and stage == 'i': # i % 2 == 0
+                        if int(h.get_shape()[1])*4 <= int(image.get_shape()[1]):
+                            h = conv2d(h, int(h.get_shape()[-1])*2, 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+                        else:
+                            h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+       
+                        print('d_h'+str(i+1)+':', h.get_shape())
+                    else:
+                        if i >= 3:
+                            if i == 3:
+                                h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta, first = True)
+                            else:
+                               h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta) 
+                        else:
+                            h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                        np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+                        print('d_h'+str(i+1)+':', h.get_shape())
+                elif feature_map_growth == 'f':
+                    if int(h.get_shape()[-1]) < z_dim:
+                        if i >= 3:
+                            if i == 3:
+                                h = conv2d(h, int(h.get_shape()[-1])*2, 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta, first = True)
+                            else:
+                                h = conv2d(h, int(h.get_shape()[-1])*2, 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta)
+                        else:
+                            h = conv2d(h, int(h.get_shape()[-1])*2, 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                        np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+                        print('d_h'+str(i+1)+':', h.get_shape())
+                    else:
+                        if i >= 3:
+                            if i == 3:
+                                h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta, first = True)
+                            else:
+                                h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                            np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME', useBeta = useBeta, beta = beta)
+                        else:
+                            h = conv2d(h, int(h.get_shape()[-1]), 3, 3, 1, 1, name='d_h'+str(i+1), stddev = 
+                        np.sqrt(2/(int(h.get_shape()[-1])*int(h.get_shape()[1])*int(h.get_shape()[2]))), padding = 'SAME')
+                        # print('d_h'+str(i+1)+':', h.get_shape())
+
+            # if bn:
+            #     d_bn = batch_norm(name='d_bn'+str(i+1))
+            #     h = d_bn(h)
+            if activation == 'lrelu':
+                h = lrelu(h)
+            elif activation == 'relu':
+                h = relu(h)
+
+        out = dense(h, 1, name = 'd_out',
+            kernel_initializer=tf.random_normal_initializer(0,stddev=np.sqrt(2/int(h.get_shape()[-1]))),
+            bias_initializer=tf.zeros_initializer(), useBeta = useBeta, beta = beta)
+
+        print('d_out:', out.get_shape())
+    return tf.nn.sigmoid(out), out
+
+
